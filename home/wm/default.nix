54ba560e704b75723a60c27ab8a5d6657420643b
Wesley Wright <wesley@wesleywright.me>
2026-09-13 11:58:29 -0400

    home/wm: set up fnott as notification daemon

--- home/wm/default.nix
+++ home/wm/default.nix
@@ -1,4 +1,8 @@
 { pkgs, ... }:
+let
+  solarized = import ./solarized.nix;
+in
+with solarized;
 {
   imports = [
     ./sway.nix
@@ -10,20 +14,57 @@
     pkgs.wl-clipboard
   ];
 
-  services.network-manager-applet.enable = true;
-  # Needed by waybar for getting currently playing status.
-  services.playerctld.enable = true;
-  # Integrates gtklock with `loginctl lock-session` and `systemctl suspend`.
-  services.swayidle = {
-    enable = true;
-    events =
-      let
-        lockCommand = "${pkgs.gtklock}/bin/gtklock";
-      in
-      {
-        "before-sleep" = lockCommand;
-        "lock" = lockCommand;
-      };
-  };
+  services = {
+    fnott = {
+      enable = true;
+      settings = {
+        main = {
+          stacking-order = "top-down";
 
+          edge-margin-horizontal = 48;
+          edge-margin-vertical = 24;
+
+          notification-margin = 12;
+
+          layer = "overlay";
+
+          background = "${base02}ff";
+          border-color = "${base01}ff";
+
+          title-font = "Noto Sans:size=11";
+          title-color = "${base0}ff";
+          title-format = "%a%A";
+
+          summary-font = "Noto Sans:size=11";
+          summary-color = "${base2}ff";
+          summary-format = "%s\n";
+
+          body-font = "Noto Sans:size=11";
+          body-color = "${base1}ff";
+          body-format = "%b";
+
+          progress-bar-height = 16;
+          progress-color = "${base1}ff";
+        };
+      };
+    };
+
+    network-manager-applet.enable = true;
+
+    # Needed by waybar for getting currently playing status.
+    playerctld.enable = true;
+
+    # Integrates gtklock with `loginctl lock-session` and `systemctl suspend`.
+    swayidle = {
+      enable = true;
+      events =
+        let
+          lockCommand = "${pkgs.gtklock}/bin/gtklock";
+        in
+        {
+          "before-sleep" = lockCommand;
+          "lock" = lockCommand;
+        };
+    };
+  };
 }
