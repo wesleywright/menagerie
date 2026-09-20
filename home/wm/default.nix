4c82e1af3bc3b5f42e6a5cda503a825fd3587737
Wesley Wright <wesley@wesleywright.me>
2026-09-20 18:32:46 -0400

    home/wm: split fnott config into new file, add max timeout for notifications

--- home/wm/default.nix
+++ home/wm/default.nix
@@ -1,10 +1,7 @@
 { pkgs, ... }:
-let
-  solarized = import ./solarized.nix;
-in
-with solarized;
 {
   imports = [
+    ./fnott.nix
     ./sway.nix
   ];
 
@@ -12,52 +9,11 @@ with solarized;
     # Take screenshots
     pkgs.sway-contrib.grimshot
 
-    # Add clipboard functionality for some apps.
+    # Allows reading and writing to and from the Wayland clipboard using the CLI.
     pkgs.wl-clipboard
   ];
 
   services = {
-    fnott = {
-      enable = true;
-      settings = {
-        main = {
-          stacking-order = "top-down";
-
-          edge-margin-horizontal = 48;
-          edge-margin-vertical = 24;
-
-          notification-margin = 12;
-
-          layer = "overlay";
-
-          background = "${base02}ff";
-          border-color = "${base01}ff";
-
-          title-font = "Noto Sans:size=11";
-          title-color = "${base0}ff";
-          title-format = "%a%A";
-
-          summary-font = "Noto Sans:size=11";
-          summary-color = "${base2}ff";
-          summary-format = "%s\n";
-
-          body-font = "Noto Sans:size=11";
-          body-color = "${base1}ff";
-          body-format = "%b";
-
-          progress-bar-height = 16;
-          progress-color = "${base1}ff";
-
-          default-timeout = 15;
-          idle-timeout = 60;
-        };
-
-        critical = {
-          default-timeout = 0;
-        };
-      };
-    };
-
     network-manager-applet.enable = true;
 
     # Needed by waybar for getting currently playing status.
---
+++ home/wm/fnott.nix
@@ -0,0 +1,51 @@
+{ pkgs, ... }:
+let
+  solarized = import ./solarized.nix;
+in
+with solarized;
+{
+  services = {
+    fnott = {
+      enable = true;
+      package = pkgs.enableDebugging pkgs.fnott;
+      settings = {
+        main = {
+          stacking-order = "top-down";
+
+          edge-margin-horizontal = 48;
+          edge-margin-vertical = 24;
+
+          notification-margin = 12;
+
+          layer = "overlay";
+
+          background = "${base02}ff";
+          border-color = "${base01}ff";
+
+          title-font = "Noto Sans:size=11";
+          title-color = "${base0}ff";
+          title-format = "%a%A";
+
+          summary-font = "Noto Sans:size=11";
+          summary-color = "${base2}ff";
+          summary-format = "%s\n";
+
+          body-font = "Noto Sans:size=11";
+          body-color = "${base1}ff";
+          body-format = "%b";
+
+          progress-bar-height = 16;
+          progress-color = "${base1}ff";
+
+          default-timeout = 15;
+          max-timeout = 60;
+          idle-timeout = 60;
+        };
+
+        critical = {
+          default-timeout = 0;
+        };
+      };
+    };
+  };
+}
