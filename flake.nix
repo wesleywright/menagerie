29240197ba1349a2afb427d383f7cbbbe5a4170a
Wesley Wright <wesley@wesleywright.me>
2026-09-23 14:45:01 -0400

    home: move home/home.nix to home/default.nix for consistency

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.naptime = import ./home/home.nix;
+              home-manager.users.naptime = import ./home;
             }
           ];
         };
