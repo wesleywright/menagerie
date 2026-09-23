1c54beee46ac99d6a74f01f1cedea6a19930f79a
Wesley Wright <wesley@wesleywright.me>
2026-09-23 15:28:03 -0400

    home/wm: tweak battery charge display

--- home/wm/sway-status-command.py
+++ home/wm/sway-status-command.py
@@ -16,8 +16,8 @@ def read_battery_charge(path: Path) -> int:
 
 def get_battery_percent(directory: Path) -> int:
     charge_now = read_battery_charge(directory / "charge_now")
-    charge_full_design = read_battery_charge(directory / "charge_full_design")
-    return 100 * charge_now // charge_full_design
+    charge_full = read_battery_charge(directory / "charge_full")
+    return 100 * charge_now // charge_full
 
 
 def format_current_time() -> str:
